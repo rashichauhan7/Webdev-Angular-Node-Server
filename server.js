5dceea620acc1f14c6862906e7079a80a9e0397a
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-08-07 17:59:17 -0400

    register username check added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://angular-node-server-db:1234abcd5678@ds031711.mlab.com:31711/heroku_10j4c4gx');
 var app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://webdev-angular-client-4.herokuapp.com, http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", 'PUT, POST, GET, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Credentials", "true");
